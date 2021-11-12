494eb9ee3213f6a3ceb870294137098ac60d50da
luftedar <ugurorcun91@gmail.com>
2021-11-12 10:32:10 +0300

    Fixed API ID

--- src/api.js
+++ src/api.js
@@ -1,15 +1,15 @@
-const id = null;
+const id = 'J7o5q6dRBeUW9kYVoON8';
 
 const createGame = () => fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games', {
   method: 'POST',
   body: JSON.stringify({
-    name: 'Orcuns Game',
+    name: 'OrcunsGame',
   }),
   headers: {
     'Content-type': 'application/json; charset=UTF-8',
   },
 })
-  .then((res) => res.json());
+  .then((res) => res.json())
 
 const getTable = () => fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores/`)
   .then((res) => res.json());
@@ -24,6 +24,5 @@ const postScore = (userInput, scoreInput) => fetch(`https://us-central1-js-capst
     'Content-type': 'application/json; charset=UTF-8',
   },
 })
-  .then((res) => res.json());
-
+  .then((res) => res.json());    
 export { createGame, getTable, postScore };
